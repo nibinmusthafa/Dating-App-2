9ed363f70ab49ae7206ced696563b08fd13cbf15
nibinm@gmail.com <nibinm@gmail.com>
2021-03-17 14:38:31 +0530

    End of Section four

---
+++ API/Data/Migrations/20210314145217_UserPasswordCanged2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class UserPasswordCanged2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Passwordsalt",
+                table: "Users",
+                newName: "PasswordSalt");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswordSalt",
+                table: "Users",
+                newName: "Passwordsalt");
+        }
+    }
+}
