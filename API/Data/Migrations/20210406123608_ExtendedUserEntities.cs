c9be93341b108a97213fec577008804a3d1e217a
nibinm@gmail.com <nibinm@gmail.com>
2021-04-06 21:15:00 +0530

    End of Section 8

---
+++ API/Data/Migrations/20210406123608_ExtendedUserEntities.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class ExtendedUserEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "url",
+                table: "Photos",
+                newName: "Url");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Url",
+                table: "Photos",
+                newName: "url");
+        }
+    }
+}
